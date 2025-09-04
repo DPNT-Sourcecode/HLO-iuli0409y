1f03926ed7a3a11ed501f3a14bfa99a3dbcdab26
sbx_user1051 <sbx_user1051@169.254.3.113>
2025-09-04 15:55:35 +0000

    Thu Sep 04 15:55:35 UTC 2025

---
+++ record_and_upload.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+SCRIPT_CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+JAVA_BIN="${SCRIPT_CURRENT_DIR}/jre/bin/java"
+JAR_FILE="${SCRIPT_CURRENT_DIR}/record/bin/record-and-upload.jar"
+PARAM_CONFIG_FILE="${SCRIPT_CURRENT_DIR}/config/credentials.config"
+PARAM_STORE_DIR="${SCRIPT_CURRENT_DIR}/record/localstore"
+PARAM_SOURCECODE_DIR="${SCRIPT_CURRENT_DIR}"
+
+# Determine which Java runtime to use based on JAVA_RUNTIME environment variable.
+if [ "${JAVA_RUNTIME}" == "system" ]; then
+  JAVA_BIN="java" # Use the Java runtime available on the PATH
+  echo "Running using system Java runtime"
+else
+  echo "Running using packaged JRE:"
+fi
+
+# For MacOS newer than 11.0.0 - allow the terminal to run the Java runtime
+xattr -rd com.apple.quarantine "${SCRIPT_CURRENT_DIR}" >/dev/null 2>&1 || true
+
+set -ex
+exec "$JAVA_BIN"                    \
+  -Dlogback.enableJansi="true"      \
+  -jar "$JAR_FILE"                  \
+  "--config" "${PARAM_CONFIG_FILE}" \
+  "--store" "${PARAM_STORE_DIR}"    \
+  "--sourcecode" "${PARAM_SOURCECODE_DIR}" "$@"
